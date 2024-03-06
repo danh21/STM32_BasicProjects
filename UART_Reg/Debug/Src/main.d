cfaf28773e9290b041c122e01a9dfb9d83ad0543
Danh Phan <phandanh2112@gmail.com>
2024-03-06 22:37:46 +0700

    UART_reg - ver 1.1: construct library

--- UART_Reg/Debug/Src/main.d
+++ UART_Reg/Debug/Src/main.d
@@ -5,7 +5,8 @@ Src/main.o: ../Src/main.c \
  D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/cmsis_compiler.h \
  D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/cmsis_gcc.h \
  D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/mpu_armv7.h \
- D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+ D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/SystemClock.h ../Inc/Timer.h ../Inc/USART.h
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/core_cm4.h:
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/cmsis_version.h:
@@ -13,3 +14,6 @@ D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/cmsis_comp
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/cmsis_gcc.h:
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Include/mpu_armv7.h:
 D:/cd/STM32/Project/STM32_BasicProjects/Led_HAL/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/SystemClock.h:
+../Inc/Timer.h:
+../Inc/USART.h:
